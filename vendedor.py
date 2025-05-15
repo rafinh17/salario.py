85a408dda0f22313c5071d434ffbb65494edf5f3
rafinh17 <fafa.macisilva15@gmail.com>
2025-05-15 17:07:01 -0300

    Add files via upload

---
+++ vendedor.py
@@ -0,0 +1,16 @@
+#definir o nome do vendedor
+nome = input ('Digite o nome do vendedor:')
+print(nome)
+#definir salario do vendedor
+salario = float(input ('Digite o salario do vendedor:'))
+print(salario)
+#definir total de vendas efetuadas
+vendas = float(input('Digite valor do total de vendas:'))
+print(vendas)
+#definir calculo de comissão
+comissao = vendas * 0.15
+print(comissao)
+#definir valor total
+total = salario + comissao
+print(total)
+
